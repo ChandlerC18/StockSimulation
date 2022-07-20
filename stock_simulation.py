667beb024267c37aaa9937a8b2b5f2d604bab3fc
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-07-19 22:50:15 -0400

    add live plot feature

--- stock_simulation.py
+++ stock_simulation.py
@@ -22,72 +22,72 @@ def login_clicked():
         title='Information',
         message=msg
     )
-
-def animate(i):
-    x = np.linspace(0, 1, 100)
-    y = fermi(x, 0.5, T[i])
-    f_d.set_data(x, y)
-    f_d.set_color(colors(i))
-    temp.set_text(str(int(T[i])) + ' K')
-    temp.set_color(colors(i))
+#
+# def animate(i):
+#     time.append(i)
+#     close.append(random.randint(0, 5))
+#     f_d.set_data(time, close)
+    # f_d.set_color(colors(i))
+    # temp.set_text(str(int(T[i])) + ' K')
+    # temp.set_color(colors(i))
 
 ### MAIN FLOW ###
-## Configure main window
-root = tk.Tk()
-
-window = (1300, 800) # window dimension
-screen = (root.winfo_screenwidth(), root.winfo_screenheight()) # get the screen dimension
-center= (int(screen[0]/2 - window[0] / 2), int(screen[1]/2 - window[1] / 2)) # find the center point
-root.geometry(f'{window[0]}x{window[1]}+{center[0]}+{center[1]}') # set position of window to center of screen
-
-# root.resizable(False, False)
-root.title('Stock Simulation')
-
-## Stock entry textbox
-stock = tk.StringVar() # store stock name
-date = tk.StringVar() # store date
-
-# # Stock entry frame
+# # Configure main window
+# root = tk.Tk()
+#
+# window = (1300, 800) # window dimension
+# screen = (root.winfo_screenwidth(), root.winfo_screenheight()) # get the screen dimension
+# center= (int(screen[0]/2 - window[0] / 2), int(screen[1]/2 - window[1] / 2)) # find the center point
+# root.geometry(f'{window[0]}x{window[1]}+{center[0]}+{center[1]}') # set position of window to center of screen
+#
+# # root.resizable(False, False)
+# root.title('Stock Simulation')
+#
+# ## Stock entry textbox
+# stock = tk.StringVar() # store stock name
+# date = tk.StringVar() # store date
+#
+# # # Stock entry frame
+# # root.columnconfigure(0, weight=1)
+# # root.columnconfigure(1, weight=2)
+# # root.rowconfigure(0, weight=1)
+# # root.rowconfigure(1, weight=2)
+# # enter_stock = ttk.Frame(root)
+# #
+# #
+# # # stock label
+# # stock_label = ttk.Label(enter_stock, text="Stock:")
+# # stock_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+# #
+# # stock_entry = ttk.Entry(enter_stock, textvariable=stock)
+# # stock_entry.focus()
+# # stock_entry.grid(column=1, row=0, sticky=tk.E, padx=5, pady=5)
+#
+#
+# # configure the grid
 # root.columnconfigure(0, weight=1)
-# root.columnconfigure(1, weight=2)
-# root.rowconfigure(0, weight=1)
-# root.rowconfigure(1, weight=2)
-# enter_stock = ttk.Frame(root)
+# root.columnconfigure(1, weight=3)
+# root.columnconfigure(2, weight=20)
 #
 #
 # # stock label
-# stock_label = ttk.Label(enter_stock, text="Stock:")
+# stock_label = ttk.Label(root, text="Stock:")
 # stock_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
 #
-# stock_entry = ttk.Entry(enter_stock, textvariable=stock)
+# stock_entry = ttk.Entry(root, textvariable=stock)
 # stock_entry.focus()
-# stock_entry.grid(column=1, row=0, sticky=tk.E, padx=5, pady=5)
-
-
-# configure the grid
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=3)
-root.columnconfigure(2, weight=20)
-
-
-# stock label
-stock_label = ttk.Label(root, text="Stock:")
-stock_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
-
-stock_entry = ttk.Entry(root, textvariable=stock)
-stock_entry.focus()
-stock_entry.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
-
-# date label
-date_label = ttk.Label(root, text="Date (YYYY-MM-DD):")
-date_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
-
-date_entry = ttk.Entry(root,  textvariable=date)
-date_entry.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
-
-# login button
-login_button = ttk.Button(root, text="Login")
-login_button.grid(column=1, row=3, sticky=tk.W, padx=5, pady=5)
+# stock_entry.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
+#
+# # date label
+# date_label = ttk.Label(root, text="Date (YYYY-MM-DD):")
+# date_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+#
+# date_entry = ttk.Entry(root,  textvariable=date)
+# date_entry.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
+#
+# # login button
+# login_button = ttk.Button(root, text="Login")
+# login_button.grid(column=1, row=3, sticky=tk.W, padx=5, pady=5)
 
 
 # ## Select date
@@ -130,16 +130,42 @@ date_start = "2022-07-07"
 date_end = "2022-07-08"
 data = yf.Ticker(tick).history(start=date_start, end=date_end, interval="1m").reset_index()
 
-data['Datetime'] = [x.strftime("%H:%M") for x in data['Datetime']]
+# data['Datetime'] = [x.strftime("%H:%M") for x in data['Datetime']]
 
-g = sns.lineplot(x='Datetime', y='Close', data=data)
-g.set_xticks(range(0, 400, 30), labels=[x for x in data['Datetime'] if x[-2:] == '00' or x[-2:] == '30'])
+# g = sns.lineplot(x='Datetime', y='Close', data=data)
+# g.set_xticks(range(0, 400, 30), labels=[x for x in data['Datetime'] if x[-2:] == '00' or x[-2:] == '30'])
+# plt.show()
+
+# time = []
+# close = []
+#
+# fig = plt.figure(figsize=(6, 4))
+# ax = fig.add_subplot(111)
+# f_d, = ax.plot([], [], linewidth=2.5)
+# temp = ax.text(1, 1, '', ha='right', va='top', fontsize=24)
+# ani = FuncAnimation(fig=fig, func=animate, frames=range(len(data)), interval=500, repeat=False)
+# fig.tight_layout()
+# plt.show()
+import random
+from itertools import count
+plt.style.use('fivethirtyeight')
+
+x_values = []
+y_values = []
+
+index = count()
+
+
+def animate(i):
+    haha = next(index)
+    x_values.append(data.iloc[haha]['Datetime']
+    y_values.append(data.iloc[haha]['Close'])
+    plt.cla()
+    plt.plot(x_values, y_values)
+
+ani = FuncAnimation(fig=plt.gcf(), func=animate, frames = len(data), interval=300, repeat=False)
+
+plt.tight_layout()
 plt.show()
-
-f_d, = ax.plot([], [], linewidth=2.5)
-temp = ax.text(1, 1, '', ha='right', va='top', fontsize=24)
-ani = FuncAnimation(fig=fig, func=animate, frames=range(len(T)), interval=500, repeat=True)
-fig.tight_layout()
-
 ## Run GUI
-root.mainloop()
+# root.mainloop()
