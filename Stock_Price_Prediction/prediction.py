ff2f34240d9ffe7b161adef68ef98707e516ce60
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-05 14:58:19 -0400

    finish model

--- Stock_Price_Prediction/prediction.py
+++ Stock_Price_Prediction/prediction.py
@@ -6,10 +6,7 @@ from matplotlib.pylab import rcParams
 from keras.models import Sequential # deep learning
 from keras.layers import LSTM, Dropout, Dense
 from sklearn.preprocessing import MinMaxScaler
-import yfinance as yf # stock data
 #---------End of imports
-
-### CLASSES ###
 
 ### FUNCTIONS ###
 
@@ -17,25 +14,71 @@ import yfinance as yf # stock data
 if __name__ == '__main__':
     rcParams['figure.figsize'] = (20, 10)
 
-    data = yf.Ticker('AAPL').history(period='max').reset_index() # get stock data
-    df = data[['Date', 'Close']]
-
-    # normalize dataset
     scaler = MinMaxScaler(feature_range=(0, 1))
-    dataset = df.values
 
-    train_data = dataset[0:987, :]
-    valid_data = dataset[987:, :]
+    df = pd.read_csv("data/NSE-Tata-Global-Beverages-Limited.csv") # read data
 
-    df.index = df.Date
-    df.drop("Date", axis=1, inplace=True)
-    scaled_data = scaler.fit_transform(dataset)
+    df["Date"] = pd.to_datetime(df.Date, format="%Y-%m-%d") # format date
+    df.index = df['Date']
 
-    x_train_data, y_train_data = [], []
+    data = df.sort_index(ascending=True, axis=0)
+    new_dataset = pd.DataFrame(index = range(0, len(df)), columns=['Date', 'Close'])
+
+    for i in range(0,len(data)):
+        new_dataset["Date"][i] = data['Date'][i]
+        new_dataset["Close"][i] = data["Close"][i]
+
+    new_dataset.index = new_dataset.Date
+    new_dataset.drop("Date", axis=1, inplace=True)
+
+    final_dataset = new_dataset.values
+
+    train_data = final_dataset[0:987, :]
+    valid_data = final_dataset[987:, :]
+
+    scaler = MinMaxScaler(feature_range = (0,1))
+    scaled_data = scaler.fit_transform(final_dataset)
+
+    x_train_data, y_train_data=[], []
 
     for i in range(60, len(train_data)):
         x_train_data.append(scaled_data[i - 60:i, 0])
         y_train_data.append(scaled_data[i, 0])
 
-x_train_data, y_train_data = np.array(x_train_data), np.array(y_train_data)
-x_train_data = np.reshape(x_train_data, (x_train_data.shape[0], x_train_data.shape[1], 1))
+    x_train_data, y_train_data = np.array(x_train_data), np.array(y_train_data)
+
+    x_train_data = np.reshape(x_train_data, (x_train_data.shape[0], x_train_data.shape[1], 1))
+
+    lstm_model=Sequential()
+    lstm_model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train_data.shape[1], 1)))
+    lstm_model.add(LSTM(units=50))
+    lstm_model.add(Dense(1))
+
+    lstm_model.compile(loss='mean_squared_error', optimizer='adam')
+    lstm_model.fit(x_train_data,y_train_data, epochs=1, batch_size=1, verbose=2)
+
+    inputs_data = new_dataset[len(new_dataset) - len(valid_data) - 60:].values
+    inputs_data = inputs_data.reshape(-1, 1)
+    inputs_data = scaler.transform(inputs_data)
+
+
+    X_test=[]
+
+    for i in range(60, inputs_data.shape[0]):
+        X_test.append(inputs_data[i - 60:i, 0])
+
+    X_test=np.array(X_test)
+
+    X_test=np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+    predicted_closing_price = lstm_model.predict(X_test)
+    predicted_closing_price = scaler.inverse_transform(predicted_closing_price)
+
+    lstm_model.save("saved_lstm_model.h5")
+
+    train_data = new_dataset[:987]
+    valid_data = new_dataset[987:]
+    valid_data['Predictions'] = predicted_closing_price
+    plt.plot(train_data["Close"])
+    plt.plot(valid_data[['Close',"Predictions"]])
+
+    plt.show()
